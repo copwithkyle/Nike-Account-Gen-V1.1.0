031ff952ba70a1fd405dfaf20d6b58fc71528278
copwithkyle <38735585+copwithkyle@users.noreply.github.com>
2018-04-25 20:17:01 +0100

    Add files via upload

---
+++ Nike Account Gen Version 1.1.0.py
@@ -0,0 +1,13 @@
+import time
+print("Nike nc/o @copwithkyle")
+accounts=[]
+print("please enter your email. This will be used to generate SIMs from getmysms.com")
+for index in range(100):
+    accounts.append(1)
+    accounts[index]=str(input("Please enter your account here"))
+    print("Successfully submitted to getmysms.com! Please wait a few seconds")
+    time.sleep(5)
+print("invalid resource. please re-enter password or download Firefox") 
+for index in range(100):
+    accounts[index]=(input("Please re-enter your accounts. If you have followed the instructions like you should have, your accounts will be verified in no time"))
+print("Thanks! All done! Please check your email. It may take a few hours but it'll be there soon")
